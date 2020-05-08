342f4b97c29ade7e962ffcf79318f0ac78cfca87
linushsao <linushsao@gmail.com>
2020-05-08 23:10:06 +0000

    foo

--- ExtraPackages/linus/stock.Analyze/R/generator.of.stockData.R
+++ ExtraPackages/linus/stock.Analyze/R/generator.of.stockData.R
@@ -8,84 +8,38 @@
 #' median_function(seq(1:10))
 
 
-generator.of.stockData <- function( select.tranSet.period=NULL, stock.data.path=NULL, list.path=NULL, group=NULL, force.update=FALSE) {
+    generator.of.stockData <- function( 
+                                        select.tranSet.period=NULL, 
+                                        stock.data.path=NULL, 
+                                        list.path=NULL, 
+                                        group=NULL, 
+                                        auto.detect=TRUE) 
+    {
 
-    generate.debug.data <- as.logical(m_env(name="generate.debug.data",mode="r"))
-    research.path.of.linus <- m_env(name="research.path.of.linus",mode="r")
+        generate.debug.data <- as.logical(m_env(name="generate.debug.data",mode="r"))
+        research.path.of.linus <- m_env(name="research.path.of.linus",mode="r")
 
-    all.code.List <- list.path
-    prefix.stock.extension <- m_env(name="prefix.stock.extension",mode="r")
-    prefix.raw.data.name <- m_env(name="prefix.raw.data.name",mode="r")
-    stock.selected.Files_Extension <- m_env(name="stock.selected.Files_Extension",mode="r")
-    prefix.debug.data.name <- "debug."
-    raw.data.Listname <- m_env(name="raw.data.Listname",mode="r")
-    debug.data.Listname <- "debug.data.List.csv"
-    debug.selected.stock.name <- c("600000.ss","600016.ss","600018.ss","600028.ss","600048.ss")
-    safe_rate <- 0.015 
-    max.record.num <- 60
-    raw.data.List <- c()
-    stock_ignore <- c("1729","2025","2384","6131","6205","6283","8201","9106") 
-    all.colname <- c("STOCK_CODE","STOCK_NAME","LAST_CLOSE","RATE","RATE.max","GROUP","TYPE","MaxDrawDOWN","SHARP_RATIO","Record_Start",'Clspr.ret.Stdev')
-    # main function
-
-    if (generate.debug.data) {
-
-#             KK_RAW <- c()
-#             
-#             name <- debug.selected.stock.name
-#             stock.cname <- debug.selected.stock.name
-#             stock.group <- "for.Debug"
-#             stock.type <- "for.Debug"
-#             
-#             for(i in 1:length(name)) {
-#                 name[i]-> symbol
-#                 file_name <- m_paste(c(stock.data.path, symbol, ".csv"), op="") 	
-#                 
-#                 tryit <- try(read.csv(file_name,header=TRUE))
-#                 
-#                 if(inherits(tryit, "try-error") ){
-#                     i <- i+1
-#                 } else {
-#                     data_RAW <- read.csv(file_name,header=TRUE)
-#                     data <- na.omit(xts(data_RAW[c(2:7)],order.by=as.Date(data_RAW$Index)))
-#                     names(data) <- c("Open","High","Low","Close","Volume","Adjusted")
-#                     
-#                     l <- length(data)
-#                     if( l == 0 ){
-#                     i <- i+1
-#                     } else {
-#                     data_1 <- data
-#                     Clspr.ret <- na.omit(ROC(data_1$Close))
-#                     return <- exp(cumsum(Clspr.ret))
-#                     mdd = maxDrawDown(cumsum(Clspr.ret))
-#                     l <- length(return)
-# 
-#                     temp <- c(symbol,stock.cname[i],data_1$Close[l],return$Close[l],stock.type,stock.group,mdd$maxdrawdow,((return$Close[l]-1-safe_rate)/mdd$maxdrawdown),as.character(index(data)[1]))
-#                     KK_RAW <- rbind(KK_RAW,temp)
-#                     }
-#                 }
-#             }
-# 
-#             debug.data.List <- paste(prefix.debug.data.name, name[1], ".csv", sep="")
-#             KK_RAW <- as.data.frame(KK_RAW)
-#             names(KK_RAW) <- all.colname
-#             write.zoo(KK_RAW, file=debug.data.List, sep=",")
-#             
-#         #save debug file list
-#         write.csv(debug.data.List,file=debug.data.Listname)
-
-        }else{
+        all.code.List <- list.path
+        prefix.stock.extension <- ifelse((group == 'global.index'), '', m_env(name="prefix.stock.extension",mode="r"))
+        prefix.raw.data.name <- m_env(name="prefix.raw.data.name",mode="r")
+        stock.selected.Files_Extension <- m_env(name="stock.selected.Files_Extension",mode="r")
+        prefix.debug.data.name <- "debug."
+        raw.data.Listname <- m_env(name="raw.data.Listname",mode="r")
+        debug.data.Listname <- "debug.data.List.csv"
+        debug.selected.stock.name <- c("600000.ss","600016.ss","600018.ss","600028.ss","600048.ss")
+        safe_rate <- 0.015 
+        max.record.num <- 60
+        raw.data.List <- c()
+        stock_ignore <- c("1729","2025","2384","6131","6205","6283","8201","9106") 
+        all.colname <- c("STOCK_CODE","STOCK_NAME","LAST_CLOSE","RATE","RATE.max","GROUP","TYPE","MaxDrawDOWN","SHARP_RATIO","Record_Start",'Clspr.ret.Stdev')
 
         KK_RAW <- c()
         if( is.null(select.tranSet.period) ) { #custom raw.data.Listname
             select.tranSet.period <- read.csv(raw.data.Listname, header=TRUE,sep=",")[,2]
             select.tranSet.period <- m_gsub(c(prefix.raw.data.name, stock.selected.Files_Extension),c(""),select.tranSet.period)
             }
-            
-#             select.tranSet.period <- as.vector(read.csv(raw.data.Listname , header=TRUE, sep=",")[,2])
 
         code.list <- read.csv(all.code.List, header=TRUE, sep=",")
-#             code.list <- code.list1
         code.list[,1] <- sapply(as.character(code.list[,1]), m_check.code)
         name <- paste(code.list[,1],prefix.stock.extension, sep = "") 
         
@@ -96,7 +50,7 @@ generator.of.stockData <- function( select.tranSet.period=NULL, stock.data.path=
         for( range in 1:length(select.tranSet.period) )  {
             selected_period <- select.tranSet.period[range]
             raw.data.List[range] <- m_paste(c(research.path.of.linus, group, '.', prefix.raw.data.name,selected_period,".csv"),op="")
-            if(file.exists(raw.data.List[range]) && !force.update) next
+            if(file.exists(raw.data.List[range]) && auto.detect) next
 
             KK_RAW <- c()
             for(i in 1:length(name)) {
@@ -105,8 +59,6 @@ generator.of.stockData <- function( select.tranSet.period=NULL, stock.data.path=
                 
                     name[i]-> symbol
                     file_name <- m_paste(c(stock.data.path, symbol,".csv"), op="") 	
-#                     tryit <- try(read.csv(file_name,header=TRUE))
-#                     if(inherits(tryit, "try-error") | (gsub(prefix.stock.extension,"",symbol) %in% stock_ignore) ){
                     if(! file.exists(file_name)) {
                         i <- i+1
                         } else {
@@ -127,25 +79,19 @@ generator.of.stockData <- function( select.tranSet.period=NULL, stock.data.path=
 
                                 temp <- c(symbol,stock.cname[i],data_1$Close[l], return$Close[l], max(return$Close), stock.type[i],stock.group[i], mdd$maxdrawdow, ((return$Close[l]-1-safe_rate)/mdd$maxdrawdown), as.vector(data_RAW$Index)[1], Clspr.ret.sd)
                                 KK_RAW <- rbind(KK_RAW,temp)
-#                                 m_msg(info=paste(selected_period, name[i], l))
                             }
                         }
                 }
-
-#                 raw.data.List[range] <- m_paste(c(research.path.of.linus, group, '.', prefix.raw.data.name,selected_period,".csv"),op="")
                 title <- all.colname
                 colnames(KK_RAW) <-  title
                 write.zoo(KK_RAW,file=raw.data.List[range],sep=",")
-                #View(KK_RAW)
             }
         #save raw file list
-#         raw.data.List.filename <- paste(research.path.of.linus, raw.data.Listname, sep="")
         write.csv(raw.data.List,file=raw.data.Listname)
+        m_env(name="raw.data.Listname", value=raw.data.Listname, mode="w")
+        
+        return( raw.data.List )
     }
-    m_env(name="raw.data.Listname", value=raw.data.Listname, mode="w")
-    return( raw.data.List )
-    
-}
 
 
 
--- ExtraPackages/linus/stock.Analyze/R/oscillation.indicator.R
+++ ExtraPackages/linus/stock.Analyze/R/oscillation.indicator.R
@@ -7,68 +7,82 @@
 #' @examples
 #' median_function(seq(1:10))
 
-        oscillation.indicator <- function(Clprs=Clprs, 
-                                    trigger.action=c('brown', 'orange') , 
-                                    trigger.mat=c(-0.005, 0.005, -0.01, 0.01, -0.015, 0.015, -0.025, 0.025, -0.04, 0.04),
-                                    record.path=NULL
+        oscillation.indicator <- function(  Clprs=Clprs, 
+                                            trigger.action=c('brown', 'orange') , 
+                                            trigger.mat=c(-0.005, 0.005, -0.01, 0.01, -0.015, 0.015, -0.025, 0.025, -0.04, 0.04),
+                                            record.path=NULL,
+                                            auto.detect=FALSE
                                     ) 
         {
             # indicator brown down through orange line, used for black swan ex. COVID VIRUS
             # indicator blue down through purple line, used for normal time
-            stock.ma <- m_time.invested( data= (Clprs) ) 
-            stock.ma <- stock.ma[complete.cases(stock.ma)]
             color.name <- c('Close','gray','gray.1','brown','blue','red')
-            names(stock.ma) <- color.name
+            if( auto.detect && !is.null(record.path) ) 
+            {
+                if( file.exists(record.path) ) 
+                {
+                    trade.summary <- list.load(record.path)
+                    stock.ma <- trade.summary[[2]]
+                }
+            
+            } else {
+                stock.ma <- m_time.invested( data= (Clprs) ) 
+                stock.ma <- stock.ma[complete.cases(stock.ma)]
+#                 color.name <- c('Close','gray','gray.1','brown','blue','red')
+                names(stock.ma) <- color.name
+            }
             
             stock.ma$clpr.ret <- ROC(stock.ma[,1])
             stock.ma <- stock.ma[complete.cases(stock.ma)]
+        
+            trigger.mat <- matrix(trigger.mat, byrow=TRUE, nrow=5, ncol=2)
+            colnames(trigger.mat) <- c('min', 'max')
+            rownames(trigger.mat) <- c('green', 'purple', 'orange', 'deep pink', 'chocolate')
             
-                trigger.mat <- matrix(trigger.mat, byrow=TRUE, nrow=5, ncol=2)
-                colnames(trigger.mat) <- c('min', 'max')
-                rownames(trigger.mat) <- c('green', 'purple', 'orange', 'deep pink', 'chocolate')
-                
-                trigger.action <- matrix(trigger.action, ncol=2) #indicator brown through level orange
-                colnames(trigger.action) <- c('indicator','trigger')
-                indicator.color <- c('darkgray','darkgray','brown','blue','red')
+            trigger.action <- matrix(trigger.action, ncol=2) #indicator brown through level orange
+            colnames(trigger.action) <- c('indicator','trigger')
+            indicator.color <- c('darkgray','darkgray','brown','blue','red')
 
-                for(trigger.id in 1:nrow(trigger.action)) {
-                trade.signal <-  c(rep(0, nrow(stock.ma)))
-                trigger.line <-  match(trigger.action[trigger.id, 'indicator'],color.name)
-                trigger.level <- match(trigger.action[trigger.id, 'trigger'],rownames(trigger.mat))
-                
-                    for(rowid in 2:nrow(stock.ma)) {
-                        
-                            if((stock.ma[rowid-1, trigger.line] > trigger.mat[trigger.level,'min']) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'min'])) {
-                                trade.signal[rowid] <- 1    #first time to down through purple.min 
-                                }                                        
-                            else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'min'])) {
-                                trade.signal[rowid] <- 1    #keep down over purple.min
-                                }                        
-                            else if((stock.ma[rowid-1, trigger.line] < trigger.mat[trigger.level,'min'])  && ((stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'min']) )) {
-                                trade.signal[rowid] <- 1    #first time up through purple.min and forward to purple.max
-                                }                                      
-                            else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'max'])) {
-                                trade.signal[rowid] <- 1    #up through min and forward to max
-                                }                       
-                            else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid-1, trigger.line] < trigger.mat[trigger.level,'max'])  && ((stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'max']) )) {
-                                trade.signal[rowid] <- 0    #first time up through purple.max
-                                }    
-                            else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'max'])) {
-                                trade.signal[rowid] <- 1    #keep up over max
-                                }                          
-
-                        }
-                }
-             
-            result <- list(trade.signal=trade.signal , stock.ma=stock.ma, trigger.action=trigger.action, indicator.color=indicator.color, trigger.line=trigger.line, trigger.mat=trigger.mat)
+            for(trigger.id in 1:nrow(trigger.action)) {
+            trade.signal <-  c(rep(0, nrow(stock.ma)))
+            trigger.line <-  match(trigger.action[trigger.id, 'indicator'],color.name)
+            trigger.level <- match(trigger.action[trigger.id, 'trigger'],rownames(trigger.mat))
             
-            if(! is.null(record.path) ) 
-            {
-                list.save(result, file=record.path)
+                for(rowid in 2:nrow(stock.ma)) {
+                    
+                        if((stock.ma[rowid-1, trigger.line] > trigger.mat[trigger.level,'min']) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'min'])) {
+                            trade.signal[rowid] <- 1    #first time to down through purple.min 
+                            }                                        
+                        else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'min'])) {
+                            trade.signal[rowid] <- 1    #keep down over purple.min
+                            }                        
+                        else if((stock.ma[rowid-1, trigger.line] < trigger.mat[trigger.level,'min'])  && ((stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'min']) )) {
+                            trade.signal[rowid] <- 1    #first time up through purple.min and forward to purple.max
+                            }                                      
+                        else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] < trigger.mat[trigger.level,'max'])) {
+                            trade.signal[rowid] <- 1    #up through min and forward to max
+                            }                       
+                        else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid-1, trigger.line] < trigger.mat[trigger.level,'max'])  && ((stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'max']) )) {
+                            trade.signal[rowid] <- 0    #first time up through purple.max
+                            }    
+                        else if((trade.signal[rowid-1] == 1) && (stock.ma[rowid, trigger.line] > trigger.mat[trigger.level,'max'])) {
+                            trade.signal[rowid] <- 1    #keep up over max
+                            }                          
+
+                    }
             }
             
-            return(result)
+        trade.signal <- xts(trade.signal, order.by=as.Date(index(stock.ma)))
+            
+        result <- list(trade.signal=trade.signal , stock.ma=stock.ma, trigger.action=trigger.action, indicator.color=indicator.color, trigger.line=trigger.line, trigger.mat=trigger.mat)
+        
+        if(!is.null(record.path) && !auto.detect) 
+        {
+            list.save(result, file=record.path)
         }
+        
+        return(result)
+    }
 
 # a <- read.csv(file='/home/linus/Project/9.Shared.Data/1_Taiwan/finance.yahoo.com/stock.price/3050.TW.csv', header=TRUE,sep=',')
 # a.xts <- xts(a[,-1], order.by=as.Date(a[,1]))
