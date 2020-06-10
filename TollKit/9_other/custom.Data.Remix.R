87f79292f921a8282f3089a11575aeece5d1fe80
linushsao <linushsao@gmail.com>
2020-06-10 00:15:59 +0000

    tweak more better

--- TollKit/9_other/custom.Data.Remix.R
+++ TollKit/9_other/custom.Data.Remix.R
@@ -13,7 +13,10 @@
     setwd(research.path.of.linus)
 
     # modify column & export
-    filename=paste0('/home/linus/Project/0_Comprehensive.Research/03_Remixed.data/01_stock/remix.stock.2020_', as.Date(Sys.time()),'.csv')
+    remix.date <- get.users.input(prompt='Pls Enter Remix.date(y-m-d), or (s)ys.time', index='remix.date')
+    if(remix.date == 's') remix.date <- as.Date(Sys.time())
+    
+    filename=paste0('/home/linus/Project/0_Comprehensive.Research/03_Remixed.data/01_stock/remix.stock.2020_', remix.date,'.csv')
     filename_remix <- paste0(gsub('.csv', '', filename), '_.csv')
     data.raw <- read.csv(filename, header=TRUE, sep=',')[,-c(1)]
     period <- c(2017, 2020)
@@ -70,6 +73,7 @@
     names(page.url) <- c('UP100', 'DN100', 'f.i.OverBought', 'f.i.OverSold')
     for(name.id in names(page.url))
     {
+#         name.id <- 'f.i.OverBought' #for test
         out.csv.raw <- page2csv(page.url=page.url[name.id], page.encoding='BIG5')
 
         out.csv <- out.csv.raw %>%
@@ -78,28 +82,73 @@
         
         if( name.id %in% c('UP100', 'DN100')) 
         {
-            data.grap <- c(1,9)
+            data.grap <- c(1,9,1)
+            out.csv <- out.csv[[data.grap[1]]][-c(1:2),-c(1)]
         }else{
-            data.grap <- c(2,6)
+            data.grap <- c(2,6,2)
+            out.csv <- out.csv[[data.grap[1]]]
         }
 #         out.csv <- out.csv[[1]][-c(1:2),-c(1)]
 #         out.csv <- out.csv[,c(1:9)]
-        out.csv <- out.csv[[data.grap[1]]][-c(1:2),-c(1)]
-        out.csv <- out.csv[,c(1:data.grap[2])]
+
+        out.csv <- out.csv[,c(data.grap[3]:data.grap[2])]
         names(out.csv) <- out.csv[1,]
         out.csv <- out.csv[-1,]
         rownames(out.csv) <- NULL
-        tmp.CodeName <- unlist(strsplit(out.csv[,1], ' '))
         
-        leng.a <- length(tmp.CodeName) / 2
-        mask <- rep(c(0,1), leng.a)
-        tmp.df <- data.frame(STOCK_DATA=tmp.CodeName, MASK=mask)
-        tmp.STOCK_CODE <- tmp.df[tmp.df$MASK==0,]
-        tmp.STOCK_NAME <- tmp.df[tmp.df$MASK==1,]
-        DnUp.check.df <- data.frame(STOCK_CODE=paste0(tmp.STOCK_CODE[,1], '.TW'), STOCK_NAME=tmp.STOCK_NAME[,1])
-        DnUp.check.df[, paste0('rank_', name.id)] <- index(DnUp.check.df)
+        if(data.grap[1]==1) 
+        {
+            tmp.CodeName <- unlist(strsplit(out.csv[,1], ' '))
+            leng.a <- length(tmp.CodeName) / 2
+            mask <- rep(c(0,1), leng.a)
+            tmp.df <- data.frame(STOCK_DATA=tmp.CodeName, MASK=mask)
+            tmp.STOCK_CODE <- tmp.df[tmp.df$MASK==0,]
+            tmp.STOCK_NAME <- tmp.df[tmp.df$MASK==1,]
+            DnUp.check.df <- data.frame(STOCK_CODE=paste0(tmp.STOCK_CODE[,1], '.TW'), STOCK_NAME=tmp.STOCK_NAME[,1])
+            DnUp.check.df[, paste0('rank_', name.id)] <- index(DnUp.check.df)
+        }else{
         
-        data.raw.tmp <- data.raw[,c('STOCK_CODE', 'STOCK_NAME', 'oscillate.brown', 'oscillate.blue', 'oscillate.red')]
+            join.vector <- function(v)
+            {
+                tmp.char <- ''
+                for(i in 1:length(v)) tmp.char <- paste0(tmp.char,v[i] )
+                return(tmp.char)
+            }
+            vec <- data.frame(v.num=c(0:9,rep(NA,16)),
+                              v.eng=c(LETTERS[seq( from = 1, to = 26 )]) )
+            tmp.code <- c()
+            for(list.row in 1:nrow(out.csv)) 
+            {
+                target <- out.csv[list.row, 1]
+                tmp.str <- c()
+                flag.jump <- FALSE
+                for(char.id in 1:nchar(target))
+                {
+                    flag.match <- FALSE
+                    for(col.id in 1:ncol(vec))
+                    {
+                        for(row.id in 1:nrow(vec))
+                        {
+                            valu <- vec[row.id, col.id]
+                            if(is.na(valu)) next            
+#                             print(c(list.row, target, valu, substr(target, char.id, char.id)))
+                            if(valu == substr(target, char.id, char.id) && !flag.jump) 
+                            {
+                                tmp.str <- c(tmp.str, valu)
+                                flag.match <- TRUE
+                            }
+                        }
+                    }
+                    if(!flag.match) flag.jump <- TRUE
+                }
+                out.csv[list.row, 1] <- gsub(join.vector(tmp.str), '', target)
+                tmp.code <- c(tmp.code, join.vector(tmp.str))
+            }
+            DnUp.check.df <- data.frame(STOCK_CODE=paste0(tmp.code, '.TW'), STOCK_NAME=out.csv[,1])
+            DnUp.check.df[, paste0('rank_', name.id)] <- index(DnUp.check.df)
+        }
+        
+        data.raw.tmp <- data.raw[,c('STOCK_CODE', 'STOCK_NAME', 'LAST_CLOSE.average', 'oscillate.brown', 'oscillate.blue', 'oscillate.red', 'oscillate.tenstion', 'oscillate.shrink')]
         data.raw.DnUp.check <- merge(data.raw.tmp, DnUp.check.df, by='STOCK_CODE', all.y=TRUE)
         data.raw.DnUp.check$STOCK_NAME.x <- data.raw.DnUp.check$STOCK_NAME.y
         data.raw.DnUp.check$STOCK_NAME.y <- NULL
@@ -111,66 +160,5 @@
         filename_remix <- paste0(gsub('.csv', '', filename), '_', name.id,'.csv')
         write.csv(data.raw.DnUp.check, file=filename_remix)
     }
-# 
-# list.path <- '/home/linus/Project/0_Comprehensive.Research/03_Remixed.data/01_stock/remix.stock.2020_2020-05-25.csv'
-# stock.list <- read.csv(list.path, header=T, sep=',')[,-c(1)]
-# stock.path <- dataset.MGR(group=c('stock','data'), request='conf')
-# stock.list$STOCK_PATH <- paste0(stock.path, stock.list$STOCK_CODE, '.csv')
-# range <- 250*3
-# col.names <- names(stock.list)
-# 
-# tmp.data <- data.frame()
-# for(stock.id in 1:length(stock.list$STOCK_CODE))
-# {
-#     data.raw <- read.csv(stock.list$STOCK_PATH[stock.id], header=T, sep=',')
-#     data.xts <- xts(data.raw[,-c(1)], order.by=as.Date(data.raw[,c(1)]))
-#     data.xts <- data.xts[complete.cases(data.xts), ]
-#     data.nrow <- nrow(data.xts)
-#     data.pre <- data.xts[(data.nrow - range):(data.nrow) ,]
-# 
-#     Clprs <- Cl(data.pre)
-#     
-#     per <- periodogram(na.omit(ROC(Clprs)), plot=FALSE)
-#     per_df <- data.frame(freq=per$freq, spec=per$spec)
-#     per.sort <- per_df[order(-per_df$spec),]
-#     per.sort$seasonal <- 1 / per.sort$freq
-#     stop()
-#     stock.mean   <- mean(Clprs) #平均數
-#     stock.median <- median(Clprs)  #中位數
-# #     stock.range <- range(Clprs) #全距
-# #     stock.quantile<- quantile(Clprs) #四分位數
-#     stock.IQR <- IQR(Clprs) #四分位差
-#     stock.sd<- sd(Clprs) #標準差
-#     stock.var <- var(Clprs) #變異數 
-#     stock.skewness <- skewness(Clprs) #偏度
-#     stock.kurtosis <- kurtosis(Clprs) #峰度   
-#     
-#     stock.summary <- c(as.character(stock.list$STOCK_CODE[stock.id]),  
-#                         stock.mean, stock.median, 
-#                         stock.IQR, stock.sd, stock.var, stock.skewness, stock.kurtosis )
-#     col.names.tmp <- c(col.names[1], 'mean', 'median', 'IQR', 'sd', 'var', 'skewness', 'kurtosis')
-#     tmp.data <- rbind(tmp.data, stock.summary)
-#     names(tmp.data) <- col.names.tmp
-#     stop()
-# }
-# write.zoo(res, file='test1.csv')
-# # etf.list <- dataset.MGR(group=c('etf','list'), request='info')[,c(1:2)]
-# # 
-# # tmp.s <- na.omit(merge(Cl(main.stock.xts), Cl(Hedge.funds.xts)))[period]
-# # res <- data.frame(cor(tmp.s))
-# # res
-# # write.zoo(res, file='test1.csv')
-# 
-# a <- read.csv('/home/linus/Project/1_R/Analysis.of.trading.strategies/all_codes.csv', header=T, sep=',')
-# b <- read.csv('/home/linus/Project/0_Comprehensive.Research/09_理論及研究資料/aa.csv', header=T, sep=',')
-# 
-# c <- merge(a,b,all=TRUE, by='STOCK_CODE')
-# c$STOCK_NAME.x <- c$STOCK_NAME.y
-# c$STOCK_NAME.y <- NULL
-# 
-# col.name <- names(c)
-# col.name <- gsub('STOCK_NAME.x', 'STOCK_NAME', col.name)
-# names(c) <- col.name
-# write.csv(c, '/home/linus/Project/1_R/Analysis.of.trading.strategies/all_codes.csv')
 
 
